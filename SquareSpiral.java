fdb317f3eeede2c9c1e0d2d4dcd6be162516688c
sim31906 <sim31906@gmail.com>
2023-06-22 22:05:39 +0700

    My first drawing

---
+++ SquareSpiral.java
@@ -0,0 +1,94 @@
+import javafx.scene.effect.Light.Distant;
+
+public class SquareSpiral {
+        public static void main(String[] args) 
+
+    {
+
+        Turtle bob = new Turtle();
+
+        {
+
+            bob.penColor("red");
+            bob.left(90);
+            bob.forward(500);
+            bob.right(90);
+            bob.forward(650);
+            bob.right(90);
+            bob.forward(500);
+            bob.left(90);
+            bob.up();
+            bob.backward(600);
+            bob.down();
+            bob.penColor("orange");
+            bob.left(90);
+            bob.forward(450);
+            bob.right(90);
+            bob.forward(550);
+            bob.right(90);
+            bob.forward(450);
+            bob.up();
+            bob.right(90);
+            bob.forward(500);
+            bob.penColor("yellow");
+            bob.down();
+            bob.right(90);
+            bob.forward(400);
+            bob.right(90);
+            bob.forward(450);
+            bob.right(90);
+            bob.forward(400);
+            bob.right(90);
+            bob.up();
+            bob.forward(400);
+            bob.down();
+            bob.penColor("green");
+            bob.right(90);
+            bob.forward(350);
+            bob.right(90);
+            bob.forward(350);
+            bob.right(90);
+            bob.forward(350);
+            bob.right(90);
+            bob.up();
+            bob.forward(300);
+            bob.down();
+            bob.penColor("light blue");
+            bob.right(90);
+            bob.forward(300);
+            bob.right(90);
+            bob.forward(250);
+            bob.right(90);
+            bob.forward(300);
+            bob.right(90);
+            bob.up();
+            bob.forward(200);
+            bob.down();
+            bob.penColor("dark blue");
+            bob.right(90);
+            bob.forward(250);
+            bob.right(90);
+            bob.forward(150);
+            bob.right(90);
+            bob.forward(250);
+            bob.right(90);
+            bob.up();
+            bob.forward(100);
+            bob.right(90);
+            bob.down();
+            bob.penColor("purple");
+            bob.forward(200);
+            bob.right(90);
+            bob.forward(50);
+            bob.right(90);
+            bob.forward(200);
+            bob.left(90);
+            bob.penColor("black");
+            bob.forward(3000);
+            bob.backward(8000);
+
+
+        }
+
+    }
+}
